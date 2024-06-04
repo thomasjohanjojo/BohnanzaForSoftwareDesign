d2e7254bce1f00fcf951bce9573f7549786fbc19
TopTargaryen <42695284+thomasjohanjojo@users.noreply.github.com>
2024-06-04 20:48:42 +0200

    Hand Class Created

---
+++ Bohnanza/src/HandClass.java
@@ -0,0 +1,32 @@
+import java.util.List;
+import java.util.Collections;
+public class HandClass implements LogicOfTheGame.Hand {
+
+    List<LogicOfTheGame.Card> listOfCardsInTheHand;
+    public LogicOfTheGame.Card getTheCardAtTheFrontOfTheHand()
+    {
+        if(listOfCardsInTheHand.size() == 0)
+        {
+            return null;
+        }
+        else
+        {
+            return listOfCardsInTheHand.removeFirst();
+        }
+    }
+
+    public void AddACardToTheBackOfTheHand(LogicOfTheGame.Card cardToAddToTheBack)
+    {
+        listOfCardsInTheHand.addLast(cardToAddToTheBack);
+    }
+
+    public void DisplayTheCardsInTheHand()
+    {
+        LogicOfTheGame.Card temporaryHolderObjectForCard;
+        for(int i = 0; i < listOfCardsInTheHand.size(); i++)
+        {
+            temporaryHolderObjectForCard = listOfCardsInTheHand.get(i);
+            System.out.print(" " + temporaryHolderObjectForCard.returnTheTypeOfTheCard() );
+        }
+    }
+}
