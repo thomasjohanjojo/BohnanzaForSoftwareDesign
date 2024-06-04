4d06dee9cc07be7aacd2347ad632ad8b8cc033ad
TopTargaryen <42695284+thomasjohanjojo@users.noreply.github.com>
2024-06-04 21:22:24 +0200

    Created the player objects

--- Bohnanza/src/Main.java
+++ Bohnanza/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args)
     {
+        for(int i = 0; i < 4; i++)
+        {
+            PlayerClass playerClassObject = new PlayerClass();
+            HandClass handClassObject = new HandClass();
+            FieldClass fieldClassObjectOne = new FieldClass();
+            FieldClass fieldClassObjectTwo = new FieldClass();
+            HandClass theAreaTokeepTheTurnedOverCards = new HandClass();
+
+            TheClassWhichHasThePlayerHandAndFields playerObject = new TheClassWhichHasThePlayerHandAndFields(playerClassObject, handClassObject, fieldClassObjectOne, fieldClassObjectTwo, theAreaTokeepTheTurnedOverCards);
+        }
 
     }
 }
